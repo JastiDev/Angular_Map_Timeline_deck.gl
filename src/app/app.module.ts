7bb74591a3a9ac0a04a681943eb43a529fa34443
jastidev <sujaneshdev@gmail.com>
2020-09-02 11:35:24 +0800

    google chart histogram added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MymapComponent } from './mymap/mymap.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TimelineComponent } from './timeline/timeline.component';
 
 import { NgxTimelineVerticalModule } from 'ngx-timeline-vertical';
+import { GoogleChartsModule } from 'angular-google-charts';
+
+import { MymapComponent } from './mymap/mymap.component';
+import { TimelineComponent } from './timeline/timeline.component';
+
+import { HistogramComponent } from './histogram/histogram.component';
 
 @NgModule({
-  declarations: [AppComponent, MymapComponent, TimelineComponent],
+  declarations: [AppComponent, MymapComponent, TimelineComponent, HistogramComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     NgxTimelineVerticalModule,
+    GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
